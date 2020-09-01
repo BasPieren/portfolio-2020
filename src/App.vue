05e29d6bd8bf62269761c9580e82ea3994d79f48
Bas Pieren <bas.pieren@quicknet.nl>
2020-09-01 11:41:15 +0200

    created <header> and <footer> components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<LayoutHeader />
+		<LayoutFooter />
+	</div>
 </template>
 
 <script>
+import LayoutHeader from './components/layout/header.vue'
+import LayoutFooter from './components/layout/footer.vue'
+
 export default {
-	name: "App"
+	name: "App",
+	components: {
+		LayoutHeader,
+		LayoutFooter
+	},
 }
 </script>
 
