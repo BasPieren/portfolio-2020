edb38358e18a62eb161cb1ca2ee35328d7f4c654
Bas Pieren <bas.pieren@quicknet.nl>
2020-09-09 12:06:47 +0200

    added checkTagType() method and tags animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
 		<LayoutHeader />
-		<Overlay />
+		<!-- <Overlay /> -->
 		<Hero />
 		<Work />
 		<LayoutFooter />
--- src/components/section/work.vue
+++ src/components/section/work.vue
@@ -12,7 +12,7 @@
 						{{ project['pay-off'] }}
 					</p>
 					<div class="work__tags">
-						<p v-for="tag in project.tags" :key="tag" class="work__tag">
+						<p v-for="tag in project.tags" :key="tag" :class="['work__tag', checkTagType(tag)]">
 							{{ tag }}
 						</p>
 					</div>
@@ -31,6 +31,19 @@ export default {
 	data() {
 		return {
 			allWork: json.work
+		}
+	},
+	methods: {
+		checkTagType(tagName){
+			if (tagName === 'Nuxt') {
+				return 'nuxt'
+			} else if (tagName === 'SCSS') {
+				return 'scss'
+			} else if (tagName === 'PHP') {
+				return 'php'
+			} else if (tagName === 'Wordpress') {
+				return 'wordpress'
+			}
 		}
 	}
 }
@@ -69,10 +82,11 @@ export default {
 		}
 		&:hover {
 			.work__title {
-				bottom: 8vw;;
+				bottom: 7vw;
 			}
 			.work__content {
 				clip-path: inset(0 0 0 0);
+				padding-bottom: 4vw;
 			}
 		}
 	}
@@ -94,7 +108,7 @@ export default {
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
-		padding: 4vw;
+		padding: 4vw 4vw 1vw 4vw;
 		height: 100%;
 		width: 100%;
 		background-color: var(--folio-purple);
@@ -119,6 +133,18 @@ export default {
 		border-radius: 1rem;
 		&::last-of-type {
 			margin: 0;
+		}
+		&.nuxt {
+			color: var(--nuxt)
+		}
+		&.scss {
+			color: var(--scss)
+		}
+		&.php {
+			color: var(--php)
+		}
+		&.wordpress {
+			color: var(--wordpress)
 		}
 	}
 }
