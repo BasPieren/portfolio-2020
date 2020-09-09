e8c1d14585b3ae74abfdb508221ac0db0af1c2e9
Bas Pieren <bas.pieren@quicknet.nl>
2020-09-09 11:37:18 +0200

    created .work__container with .work__item elements

--- src/components/section/work.vue
+++ src/components/section/work.vue
@@ -3,27 +3,31 @@
 		<h3>Werk.</h3>
 		<div class="work__container">
 			<div v-for="project in allWork" :key="project" class="work__item">
-				<p class="work__copy">
-					{{ project['pay-off'] }}
-				</p>
+				<img :src="require(`../../static/images/${project.images[0].src}/${project.images[0].name}`).default" :alt="project.images[0].alt" class="work__image">
 				<h4 class="work__title">
 					{{ project.name }}
 				</h4>
-				<div class="work__tags">
-					<p v-for="tag in project.tags" :key="tag" class="work__tag">
-						{{ tag }}
+				<div class="work__content">
+					<p class="work__copy">
+						{{ project['pay-off'] }}
 					</p>
+					<div class="work__tags">
+						<p v-for="tag in project.tags" :key="tag" class="work__tag">
+							{{ tag }}
+						</p>
+					</div>
 				</div>
-				<!-- <img :src="require(`./images/${projecy.images[0].name}`)" :alt="project.images[0].alt" class="work__image"> -->
 			</div>
 		</div>
 	</section>
 </template>
 
+
 <script>
 import json from '../../static/json/work.json'
+
 export default {
-	name: "Header",
+	name: "Work",
 	data() {
 		return {
 			allWork: json.work
@@ -51,17 +55,57 @@ export default {
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
-		padding: 4vw;
 		background-color: var(--folio-purple);
 		overflow: hidden;
+		&::after {
+			content: '';
+			position: absolute;
+			display: block;
+			width: 100%;
+			height: 100%;
+			bottom: 0;
+			left: 0;
+			background-image: linear-gradient(180deg,transparent,rgba(0,0,0,.4));
+		}
+		&:hover {
+			.work__title {
+				bottom: 8vw;;
+			}
+			.work__content {
+				clip-path: inset(0 0 0 0);
+			}
+		}
+	}
+	&__image {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	&__title {
+		position: absolute;
+		bottom: 4vw;
+		left: 4vw;
+		color: var(--light);
+		transition: 0.4s;
+		z-index: 3;
+	}
+	&__content {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 4vw;
+		height: 100%;
+		width: 100%;
+		background-color: var(--folio-purple);
+		clip-path: inset(100% 0 0 0);
+		transition: 0.4s;
+		z-index: 2;
 	}
 	&__copy {
 		color: var(--light);
 		font-style: italic;
 		opacity: 0.5;
-	}
-	&__title {
-		margin: auto 0 1rem 0;
 	}
 	&__tags {
 		display: flex;
@@ -76,9 +120,6 @@ export default {
 		&::last-of-type {
 			margin: 0;
 		}
-	}
-	&__image {
-		position: absolute;
 	}
 }
 </style>
