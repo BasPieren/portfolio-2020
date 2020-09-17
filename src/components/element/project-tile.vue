f615bd2b2a2fc254a34e820aca4b42dbab6ae8d7
Bas Pieren <bas.pieren@quicknet.nl>
2020-09-17 13:07:43 +0200

    seperated .project-tile from .work.vue into element

---
+++ src/components/element/project-tile.vue
@@ -0,0 +1,151 @@
+<template>
+	<div class="project-tile">
+		<router-link :to="project.url" class="project-tile__link" @click.native="updateHeader(); updateHeroStore(project); updateActiveProject(project);"></router-link>
+		<img :src="require(`../../static/images/${project.images[0].src}/${project.images[0].name}`).default" :alt="project.images[0].alt" class="project-tile__image">
+		<h3 class="project-tile__title">
+			{{ project.name }}
+		</h3>
+		<div class="project-tile__content">
+			<p class="project-tile__copy">
+				{{ project['pay-off'] }}
+			</p>
+			<div class="project-tile__tags">
+				<p v-for="tag in project.tags" :key="tag" :class="['project-tile__tag', checkTagType(tag)]">
+					{{ tag }}
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ProjectTile",
+	props: {
+		project: {
+			default: () => {},
+			type: Object
+		}
+	},
+	methods: {
+		checkTagType(tagName){
+			if (tagName === 'Nuxt') {
+				return 'nuxt'
+			} else if (tagName === 'SCSS') {
+				return 'scss'
+			} else if (tagName === 'PHP') {
+				return 'php'
+			} else if (tagName === 'Wordpress') {
+				return 'wordpress'
+			}
+		},
+		updateHeroStore(payload) {
+			this.$store.commit('updateHero', payload)
+		},
+		updateHeader() {
+			this.$store.commit('updateHeader')
+		},
+		updateActiveProject(payload) {
+			this.$store.commit('updateActiveProject', payload)
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.project-tile {
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	background-color: var(--folio-purple);
+	overflow: hidden;
+	&::after {
+		content: '';
+		position: absolute;
+		display: block;
+		width: 100%;
+		height: 100%;
+		bottom: 0;
+		left: 0;
+		background-image: linear-gradient(180deg,transparent,rgba(0,0,0,.4));
+	}
+	&:hover {
+		.project-tile__title {
+			bottom: 7vw;
+		}
+		.project-tile__content {
+			clip-path: inset(0 0 0 0);
+			padding-bottom: 4vw;
+		}
+	}
+	&__image {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	&__title {
+		position: absolute;
+		bottom: 4vw;
+		left: 4vw;
+		color: var(--light);
+		transition: 0.4s;
+		z-index: 3;
+	}
+	&__content {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 4vw 4vw 1vw 4vw;
+		height: 100%;
+		width: 100%;
+		background-color: var(--folio-purple);
+		clip-path: inset(100% 0 0 0);
+		transition: 0.4s;
+		z-index: 2;
+	}
+	&__link {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		z-index: 3;
+	}
+	&__copy {
+		color: var(--light);
+		font-style: italic;
+		opacity: 0.5;
+	}
+	&__tags {
+		display: flex;
+		width: 100%;
+	}
+	&__tag {
+		font-family: Roboto, sans-serif;
+		background-color: var(--light);
+		margin: 0 0.5rem 0 0;
+		padding: 0.5rem 1rem;
+		border-radius: 1rem;
+		&::last-of-type {
+			margin: 0;
+		}
+		&.nuxt {
+			color: var(--nuxt)
+		}
+		&.scss {
+			color: var(--scss)
+		}
+		&.php {
+			color: var(--php)
+		}
+		&.wordpress {
+			color: var(--wordpress)
+		}
+	}
+	&__image {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+}
+</style>
--- src/components/section/work.vue
+++ src/components/section/work.vue
@@ -4,57 +4,23 @@
 			Werk.
 		</h3>
 		<div class="work__container">
-			<div v-for="project in allWork" :key="project.name" class="work__item">
-				<router-link :to="project.url" class="work__link" @click.native="updateHeader(); updateHeroStore(project); updateActiveProject(project);"></router-link>
-				<img :src="require(`../../static/images/${project.images[0].src}/${project.images[0].name}`).default" :alt="project.images[0].alt" class="work__image">
-				<h3 class="work__title">
-					{{ project.name }}
-				</h3>
-				<div class="work__content">
-					<p class="work__copy">
-						{{ project['pay-off'] }}
-					</p>
-					<div class="work__tags">
-						<p v-for="tag in project.tags" :key="tag" :class="['work__tag', checkTagType(tag)]">
-							{{ tag }}
-						</p>
-					</div>
-				</div>
-			</div>
+			<ProjectTile v-for="project in allProjects" :key="project.name" :project="project" />
 		</div>
 	</section>
 </template>
 
 <script>
 import json from '@/static/json/work.json'
+import ProjectTile from '@/components/element/project-tile.vue'
 
 export default {
 	name: "Work",
+	components: {
+		ProjectTile
+	},
 	data() {
 		return {
-			allWork: json.work
-		}
-	},
-	methods: {
-		checkTagType(tagName){
-			if (tagName === 'Nuxt') {
-				return 'nuxt'
-			} else if (tagName === 'SCSS') {
-				return 'scss'
-			} else if (tagName === 'PHP') {
-				return 'php'
-			} else if (tagName === 'Wordpress') {
-				return 'wordpress'
-			}
-		},
-		updateHeroStore(payload) {
-			this.$store.commit('updateHero', payload)
-		},
-		updateHeader() {
-			this.$store.commit('updateHeader')
-		},
-		updateActiveProject(payload) {
-			this.$store.commit('updateActiveProject', payload)
+			allProjects: json.work
 		}
 	}
 }
@@ -85,96 +51,6 @@ export default {
 			grid-template-columns: 1fr;
 			grid-gap: 1rem;
 			padding: 2rem 0 0 0;
-		}
-	}
-	&__item {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		background-color: var(--folio-purple);
-		overflow: hidden;
-		&::after {
-			content: '';
-			position: absolute;
-			display: block;
-			width: 100%;
-			height: 100%;
-			bottom: 0;
-			left: 0;
-			background-image: linear-gradient(180deg,transparent,rgba(0,0,0,.4));
-		}
-		&:hover {
-			.work__title {
-				bottom: 7vw;
-			}
-			.work__content {
-				clip-path: inset(0 0 0 0);
-				padding-bottom: 4vw;
-			}
-		}
-	}
-	&__image {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-	&__title {
-		position: absolute;
-		bottom: 4vw;
-		left: 4vw;
-		color: var(--light);
-		transition: 0.4s;
-		z-index: 3;
-	}
-	&__content {
-		position: absolute;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		padding: 4vw 4vw 1vw 4vw;
-		height: 100%;
-		width: 100%;
-		background-color: var(--folio-purple);
-		clip-path: inset(100% 0 0 0);
-		transition: 0.4s;
-		z-index: 2;
-	}
-	&__link {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		z-index: 3;
-	}
-	&__copy {
-		color: var(--light);
-		font-style: italic;
-		opacity: 0.5;
-	}
-	&__tags {
-		display: flex;
-		width: 100%;
-	}
-	&__tag {
-		font-family: Roboto, sans-serif;
-		background-color: var(--light);
-		margin: 0 0.5rem 0 0;
-		padding: 0.5rem 1rem;
-		border-radius: 1rem;
-		&::last-of-type {
-			margin: 0;
-		}
-		&.nuxt {
-			color: var(--nuxt)
-		}
-		&.scss {
-			color: var(--scss)
-		}
-		&.php {
-			color: var(--php)
-		}
-		&.wordpress {
-			color: var(--wordpress)
 		}
 	}
 }
