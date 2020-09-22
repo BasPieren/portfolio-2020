9462119987a00ddd7394ebb0818d30f6c4eaec79
Bas Pieren <bas.pieren@quicknet.nl>
2020-09-22 14:51:58 +0200

    installed vue-scrollmagic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import VueSvgIcon from 'vue-svgicon'
 import VueScrollTo from 'vue-scrollto'
+import VueScrollmagic from 'vue-scrollmagic'
 import router from '@/router/index.js'
 import store from '@/store/index.js'
 import '@/assets/app.scss'
@@ -11,6 +12,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueSvgIcon, { tagName: 'svgicon' })
 Vue.use(VueScrollTo)
+Vue.use(VueScrollmagic)
 
 new Vue({
 	router,
